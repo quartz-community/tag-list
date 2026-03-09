29da63c8e71abb0ca052f2867e2acf5bb11e5945
saberzero1 <github@emilebangma.com>
2026-03-09 03:04:02 +0100

    deps: update @quartz-community/types and @quartz-community/utils
    Also fix code formatting with prettier.

--- src/util/lang.ts
+++ src/util/lang.ts
@@ -1,5 +1,3 @@
-export function classNames(
-  ...classes: (string | undefined | null | false)[]
-): string {
+export function classNames(...classes: (string | undefined | null | false)[]): string {
   return classes.filter(Boolean).join(" ");
 }
